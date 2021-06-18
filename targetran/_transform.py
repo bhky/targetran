d1ae18a22717ca623226508439303b2ae2a542a1
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-06-18 18:33:19 +0800

    Add rotate 90 clockwise func

--- targetran/_transform.py
+++ targetran/_transform.py
@@ -66,3 +66,38 @@ def _flip_up_down(
     bboxes_list = split_func(all_bboxes, indices, 0)  # Along axis 0.
 
     return images, [reshape_func(bboxes, (-1, 4)) for bboxes in bboxes_list]
+
+
+def _rotate_90_clockwise(
+        images: np.ndarray,
+        bboxes_list: List[np.ndarray],
+        shape_func: Callable[[T], Tuple[int, ...]],
+        transpose_func: Callable[[T, Tuple[int, ...]], T],
+        concat_func: Callable[[List[T], int], T],
+        split_func: Callable[[T, T, int], List[T]],
+        reshape_func: Callable[[T, Tuple[int, int]], T]
+) -> Tuple[np.ndarray, List[np.ndarray]]:
+    """
+    images: [bs, h, w, c]
+    bboxes (for one image): [[top_left_x, top_left_y, width, height], ...]
+    """
+    assert shape_func(images)[-1] == 3
+    image_height = shape_func(images)[1]
+
+    images = transpose_func(images, (0, 2, 1, 3))[:, :, ::-1, :]
+
+    all_bboxes = concat_func(bboxes_list, 0)  # Along axis 0.
+    assert shape_func(all_bboxes)[-1] == 4
+
+    all_bboxes = concat_func([
+        image_height - all_bboxes[:, 1:2] - all_bboxes[:, 3:],
+        all_bboxes[:, :1],
+        all_bboxes[:, 3:],
+        all_bboxes[:, 2:3],
+    ], 1)  # Along axis 1.
+
+    bboxes_nums = [len(bboxes) for bboxes in bboxes_list]
+    indices = np.cumsum(bboxes_nums)[:-1]
+    bboxes_list = split_func(all_bboxes, indices, 0)  # Along axis 0.
+
+    return images, [reshape_func(bboxes, (-1, 4)) for bboxes in bboxes_list]
