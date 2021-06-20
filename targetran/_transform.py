2aff8b1418d9c1f99d8d943d0ae3633d2acb9716
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-06-20 15:07:09 +0800

    Images must be 4D

--- targetran/_transform.py
+++ targetran/_transform.py
@@ -36,7 +36,10 @@ def _flip_left_right(
     images: [bs, h, w, c]
     bboxes (for one image): [[top_left_x, top_left_y, width, height], ...]
     """
-    image_width = shape_func(images)[2]
+    image_shape = shape_func(images)
+    assert len(image_shape) == 4
+
+    image_width = image_shape[2]
 
     images = images[..., ::-1, :]
 
@@ -65,7 +68,10 @@ def _flip_up_down(
     images: [bs, h, w, c]
     bboxes (for one image): [[top_left_x, top_left_y, width, height], ...]
     """
-    image_height = shape_func(images)[1]
+    image_shape = shape_func(images)
+    assert len(image_shape) == 4
+
+    image_height = image_shape[1]
 
     images = images[:, ::-1, ...]
 
@@ -95,7 +101,10 @@ def _rotate_90_clockwise(
     images: [bs, h, w, c]
     bboxes (for one image): [[top_left_x, top_left_y, width, height], ...]
     """
-    image_height = shape_func(images)[1]
+    image_shape = shape_func(images)
+    assert len(image_shape) == 4
+
+    image_height = image_shape[1]
 
     images = transpose_func(images, (0, 2, 1, 3))[:, :, ::-1, :]
 
