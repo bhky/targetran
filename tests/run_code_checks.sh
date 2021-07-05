6f05803c234a77c5e70721aad378f11d55e08ccf
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-07-05 12:50:59 +0800

    Add --exit-zero to pylint so that the whole check script could run

--- tests/run_code_checks.sh
+++ tests/run_code_checks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint
+find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --exit-zero
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 python3 -m unittest
