c026a07c01b0e2f55447914dab48ead2c17d88fa
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-07-30 10:19:48 +0800

    Allow implicit-reexport in mypy

--- tests/run_code_checks.sh
+++ tests/run_code_checks.sh
@@ -2,5 +2,5 @@
 set -e
 
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --exit-zero
-find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs
+find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs --implicit-reexport
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 python3 -m unittest
