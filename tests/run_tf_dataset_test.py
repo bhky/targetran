3745bf4396ca3fd566e4f6721d45ec0fd3cb9d9c
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-08-02 16:02:35 +0800

    Included padded_batch demo

--- tests/run_tf_dataset_test.py
+++ tests/run_tf_dataset_test.py
@@ -25,13 +25,15 @@ def make_np_data() -> Tuple[Sequence[np.ndarray],
         np.array([], dtype=np.float32).reshape(-1, 4),
         np.array([
             [104, 151, 22, 10],
+            [99, 132, 20, 15],
+            [340, 220, 31, 12],
         ], dtype=np.float32),
     ]
 
     labels_list = [
         np.array([0, 1], dtype=np.float32),
         np.array([], dtype=np.float32),
-        np.array([2], dtype=np.float32),
+        np.array([2, 3, 0], dtype=np.float32),
     ]
 
     return image_list, bboxes_list, labels_list
@@ -64,16 +66,27 @@ def main() -> None:
     ds = ds.map(lambda i, b, l: (i.to_tensor(), b.to_tensor(), l))
     ds = ds \
         .map(targetran.tf.TFRandomRotate(probability=1.0)) \
-        .map(targetran.tf.TFRandomCrop(probability=1.0)) \
         .map(targetran.tf.TFRandomFlipUpDown(probability=1.0))
 
     for sample in ds:
         image, bboxes, labels = sample
         print(f"transformed image shape: {image.get_shape()}")
         print(f"transformed bboxes shape: {bboxes.get_shape()}")
-        print(f"transformed bboxes: {bboxes}")
+        print(f"transformed bboxes: {bboxes.numpy().tolist()}")
         print(f"transformed labels shape: {labels.get_shape()}")
-        print(f"transformed labels: {labels}")
+        print(f"transformed labels: {labels.numpy().tolist()}")
+
+    print("-------- Batching --------")
+
+    ds = ds.padded_batch(2, padding_values=np.nan)
+
+    for batch in ds:
+        image_batch, bboxes_batch, labels_batch = batch
+        print(f"transformed image batch shape: {image_batch.get_shape()}")
+        print(f"transformed bboxes batch shape: {bboxes_batch.get_shape()}")
+        print(f"transformed bboxes batch: {bboxes_batch.numpy().tolist()}")
+        print(f"transformed labels batch shape: {labels_batch.get_shape()}")
+        print(f"transformed labels batch: {labels_batch.numpy().tolist()}")
 
 
 if __name__ == "__main__":
