b06b622e75dd8efe7aa00c33323cbb582c648c79
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-08-24 08:48:58 +0800

    Revert "Fix paths"
    This reverts commit 9ccc04813e079c83abbe7675179a0b727f56012b.

--- tests/install_from_local_and_test.sh
+++ tests/install_from_local_and_test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-./install_from_locals.sh
-./run_code_checks.sh
+./tests/install_from_locals.sh
+./tests/run_code_checks.sh
 
-python3 ./run_pt_dataset_test.py
-python3 ./run_tf_dataset_test.py
+python3 ./tests/run_pt_dataset_test.py
+python3 ./tests/run_tf_dataset_test.py
