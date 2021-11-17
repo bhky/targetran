e21fc3ad0da22ac37697099087d51e8be4d19b8f
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-11-17 16:12:22 +0800

    Stricter mypy inspection also to the tests dir

--- tests/run_code_checks.sh
+++ tests/run_code_checks.sh
@@ -3,5 +3,8 @@ set -e
 
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --errors-only
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --exit-zero
-find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs --implicit-reexport
+find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs
+find tests -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --errors-only
+find tests -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --exit-zero
+find tests -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs
 find tests -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 python3 -m unittest
