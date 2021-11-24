01d97ceae2923bca217116f92925409089cc97f8
Bosco Yung <15840328+bhky@users.noreply.github.com>
2021-11-24 14:31:29 +0800

    Clean up

--- targetran/_typing.py
+++ targetran/_typing.py
@@ -9,9 +9,9 @@ import numpy.typing
 
 ArrayLike = np.typing.ArrayLike
 NDAnyArray = np.typing.NDArray[Any]
+NDBoolArray = np.typing.NDArray[np.bool_]
 NDFloatArray = np.typing.NDArray[np.float_]
 NDIntArray = np.typing.NDArray[np.int_]
-NDBoolArray = np.typing.NDArray[np.bool_]
 
 # T is treated semantically as "NDAnyArray or tf.Tensor" in this library.
 # However, there is currently no way to express this accurately in the
