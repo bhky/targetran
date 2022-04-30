072f1ad908a699b357d8f2b88c0cc3de042f8197
Bosco Yung <15840328+bhky@users.noreply.github.com>
2022-04-30 10:12:36 +0800

    The tests dir does not need to be a package

--- tests/run_code_checks.sh
+++ tests/run_code_checks.sh
@@ -4,4 +4,4 @@ set -e
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --errors-only
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --exit-zero
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs
-find tests -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 python3 -m unittest
+find tests -iname "*.py" | xargs -L 1 python3 -m unittest
