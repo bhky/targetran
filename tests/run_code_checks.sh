b5d0d210e01b34a694865d101e507567ce6edfbc
Bosco Yung <15840328+bhky@users.noreply.github.com>
2023-03-30 10:16:19 +0900

    Fix axis grid for plotting test examples

--- tests/run_code_checks.sh
+++ tests/run_code_checks.sh
@@ -3,5 +3,5 @@ set -e
 
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --errors-only
 find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 pylint --exit-zero
-find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs
+find targetran -iname "*.py" | grep -v -e "__init__.py" | xargs -L 1 mypy --strict --check-untyped-defs --disable-error-code empty-body
 find tests -iname "*.py" | xargs -L 1 python3 -m unittest
