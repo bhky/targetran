6984525b93fabb91869f92501c19856bbd4eff42
Bosco Yung <15840328+bhky@users.noreply.github.com>
2025-01-24 18:31:07 +0900

    NDFloatArray = NDAnyArray

--- targetran/_typing.py
+++ targetran/_typing.py
@@ -9,7 +9,7 @@ import numpy.typing
 ArrayLike = np.typing.ArrayLike
 NDAnyArray = np.typing.NDArray[Any]
 NDBoolArray = np.typing.NDArray[np.bool_]
-NDFloatArray = np.typing.NDArray[np.generic]
+NDFloatArray = NDAnyArray  # Not nice, but since NumPy 2.0 there's no better way yet.
 NDIntArray = np.typing.NDArray[np.int_]
 
 # T is treated semantically as "NDAnyArray or tf.Tensor" in this library.
